7292bf04581f232c024a4e5ea953bd278c6a0bd6
josegonzaleza <josegonzaleza@uned.cr>
2026-02-21 23:44:51 -0600

    Programacion de clase de site.master

--- Tarea1/Site.Master.cs
+++ Tarea1/Site.Master.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Tarea1.AppCode;
+using static Tarea1.AppCode.Models;
 
 namespace Tarea1
 {
@@ -11,7 +13,37 @@ namespace Tarea1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataStore.EnsureSeed();
 
+            var user = Session["currentUser"] as User;
+
+            bool isLoggedIn = user != null;
+            lnkPrincipal.Visible = isLoggedIn;
+            btnLogout.Visible = isLoggedIn;
+
+            if (!isLoggedIn)
+            {
+                lnkGestion.Visible = false;
+                lnkActividades.Visible = false;
+                return;
+            }
+
+            if (user.role == UserRole.Admin)
+            {
+                lnkGestion.Visible = true;
+                lnkActividades.Visible = false;
+            }
+            else
+            {
+                lnkGestion.Visible = false;
+                lnkActividades.Visible = true;
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Remove("currentUser");
+            Response.Redirect("~/Login.aspx");
         }
     }
 }
